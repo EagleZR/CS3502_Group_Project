cd8effd492f334d2680e99949fe910bd2151bbda
Mark Zeagler <mark.zeagler@outlook.com>
2018-02-13 09:51:38 -0500

    IntToHex correction

--- Yeezus/src/IntToHex.java
+++ Yeezus/src/IntToHex.java
@@ -1,11 +1,9 @@
-import java.util.function.Function;
-
 /**
  * Converts an {@link Integer} to its hex value.
  *
  * @version 1.0
  */
-public class IntToHex implements Function<Integer, String> {
+public class IntToHex {
 
 	/**
 	 * Accepts an integer and returns the value displayed in hexadecimal.
@@ -13,7 +11,7 @@ public class IntToHex implements Function<Integer, String> {
 	 * @param integer The integer whose value is to be displayed in hexadecimal.
 	 * @return The hexadecimal value of the given integer.
 	 */
-	@Override public String apply( Integer integer ) {
+	public static String convert( Integer integer ) {
 		// https://stackoverflow.com/questions/13851743/how-to-format-numbers-to-a-hex-strings
 		return String.format( "0x%08X", integer );
 	}
