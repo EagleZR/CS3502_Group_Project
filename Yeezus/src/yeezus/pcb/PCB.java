6d4dbeb48e044486b735d260faa8cbaa03114562
Mark Zeagler <mark.zeagler@outlook.com>
2018-02-19 15:57:43 -0500

    PCB Setup

--- Yeezus/src/yeezus/pcb/PCB.java
+++ Yeezus/src/yeezus/pcb/PCB.java
@@ -4,4 +4,9 @@ import java.util.ArrayList;
 
 public class PCB extends ArrayList<Process> { // TODO Do it like this?
 
+	public void addProcess( int pid, int startInstructionAddress, int endInstructionAddress,
+			int startInputBufferAddress, int endInputBufferAddress, int startOutputBufferAddress,
+			int endOutputBufferAddress, int startTempBufferAddress, int endTempBufferAddress ) {
+
+	}
 }
