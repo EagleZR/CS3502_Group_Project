ab735d001f3c828ec8bdc73a891207f3672b521b
Mark Zeagler <mark.zeagler@outlook.com>
2018-03-01 09:14:28 -0500

    Added all test cases to Test_All

--- Yeezus/src/yeezus/Test_All.java
+++ Yeezus/src/yeezus/Test_All.java
@@ -6,6 +6,7 @@ import yeezus.cpu.Test_Decoder;
 import yeezus.cpu.Test_InstructionSet;
 import yeezus.driver.Test_File;
 import yeezus.driver.Test_Loader;
+import yeezus.driver.Test_Scheduler;
 import yeezus.memory.Test_Memory;
 import yeezus.memory.Test_Word;
 import yeezus.pcb.Test_TaskManager;
@@ -13,5 +14,5 @@ import yeezus.pcb.Test_PCB;
 
 @RunWith( Suite.class ) @Suite.SuiteClasses( { Test_File.class, Test_InstructionSet.class, Test_Memory.class,
 		Test_TaskManager.class, Test_PCB.class, Test_Decoder.class, Test_Word.class,
-		Test_Loader.class } ) public class Test_All {
+		Test_Loader.class, Test_Scheduler.class } ) public class Test_All {
 }
