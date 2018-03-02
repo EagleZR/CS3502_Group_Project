8291b2011788eb4a553d42c417f8fad4044890f7
Mark Zeagler <mark.zeagler@outlook.com>
2018-03-02 15:10:57 -0500

    And broken Decoder test

--- Yeezus/src/yeezus/cpu/Test_Decoder.java
+++ Yeezus/src/yeezus/cpu/Test_Decoder.java
@@ -1,6 +1,7 @@
 package yeezus.cpu;
 
 import org.junit.Test;
+import yeezus.memory.MMU;
 import yeezus.memory.Memory;
 import yeezus.memory.Word;
 import yeezus.pcb.TaskManager;
@@ -14,7 +15,7 @@ import static org.junit.Assert.assertEquals;
 public class Test_Decoder {
 
 	@Test public void decode() throws Exception {
-		CPU cpu = new CPU( new TaskManager(), new Memory( 16 ) );
+		CPU cpu = new CPU( new MMU( new Memory( 1024 ) ), new Memory( 16 ) );
 		ExecutableInstruction executableInstruction = cpu.decode( new Word( "0xC050005C" ) );
 		assertEquals( InstructionSet.RD, executableInstruction.type );
 		assertEquals( ExecutableInstruction.IOExecutableInstruction.class, executableInstruction.getClass() );
@@ -23,7 +24,7 @@ public class Test_Decoder {
 
 	@Test public void add() throws Exception {
 		Memory registers = new Memory( 16 );
-		CPU cpu = new CPU( new TaskManager(), registers );
+		CPU cpu = new CPU( new MMU( new Memory( 1024 ) ), registers );
 		registers.write( 7, new Word( "0x2341" ) );
 		cpu.decode( new Word( "0x05070000" ) ).execute();
 		assertEquals( "0x00002341", registers.read( 0 ).toString() );
