938a1bcf81de082327d0084a2465dbca198ca8f2
jessbrummel <jessbrummel@hotmail.com>
2018-03-05 16:17:38 -0500

    DMAChannel
    rough drafting the DMA channel yo

--- Yeezus/src/yeezus/cpu/DMAChannel.java
+++ Yeezus/src/yeezus/cpu/DMAChannel.java
@@ -1,5 +1,6 @@
 package yeezus.cpu;
 
+import yeezus.memory.InvalidAddressException;
 import yeezus.memory.MMU;
 import yeezus.memory.Memory;
 import yeezus.pcb.PCB;
@@ -9,11 +10,27 @@ public class DMAChannel {
 	Memory RAM;
 	Memory registers;
 
-	public DMAChannel( MMU mmu, Memory registers ) {
+	MMU mmu;
+	int pid;
 
+	public DMAChannel( MMU mmu, Memory registers ) {
+		this.mmu = mmu;
+		this.registers = registers;
 	}
 
-	public void handle( ExecutableInstruction.IOExecutableInstruction instruction, PCB pcb ) {
+	public void handle( ExecutableInstruction.IOExecutableInstruction instruction, PCB pcb ) throws InvalidAddressException {
+		pid = pcb.getPid();
 
+		//read
+		if(instruction.type == InstructionSet.RD)
+		{
+			registers.write(instruction.reg1, mmu.read(pid, instruction.address) );
+		}
+
+		//write
+		if(instruction.type == InstructionSet.WR)
+		{
+			mmu.write(pid, instruction.address, registers.read(instruction.reg1) );
+		}
 	}
 }
