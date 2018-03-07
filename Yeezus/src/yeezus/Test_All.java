2c7fb8e58c02ee0d48d1516f5d622df1023bba05
Mark Zeagler <mark.zeagler@outlook.com>
2018-03-07 16:12:31 -0500

    Updated Test_All

--- Yeezus/src/yeezus/Test_All.java
+++ Yeezus/src/yeezus/Test_All.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import yeezus.cpu.Test_Decoder;
 import yeezus.cpu.Test_InstructionSet;
+import yeezus.driver.Test_Dispatcher;
 import yeezus.driver.Test_File;
 import yeezus.driver.Test_Loader;
 import yeezus.driver.Test_Scheduler;
@@ -14,5 +15,5 @@ import yeezus.pcb.Test_PCB;
 
 @RunWith( Suite.class ) @Suite.SuiteClasses( { Test_File.class, Test_InstructionSet.class, Test_Memory.class,
 		Test_TaskManager.class, Test_PCB.class, Test_Decoder.class, Test_Word.class,
-		Test_Loader.class, Test_Scheduler.class } ) public class Test_All {
+		Test_Loader.class, Test_Scheduler.class, Test_Dispatcher.class } ) public class Test_All {
 }
