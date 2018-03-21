0b22e348e03f7f970b4cc50c0a4e1596c0ff70db
Mark Zeagler <mark.zeagler@outlook.com>
2018-03-21 13:28:47 -0400

    Threading GUI

--- Yeezus/src/yeezus/gui/GUI.java
+++ Yeezus/src/yeezus/gui/GUI.java
@@ -1,5 +1,6 @@
 package yeezus.gui;
 
+import com.sun.istack.internal.NotNull;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
@@ -13,18 +14,22 @@ import yeezus.memory.Word;
 import yeezus.pcb.TaskManager;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class GUI extends Application {
 
-	Memory disk;
-	Memory RAM;
-	Memory registers;
-	MMU mmu;
+	private Memory disk;
+	private Memory RAM;
+	private Memory registers;
+	private MMU mmu;
 
-	CPUSchedulingPolicy schedulingPolicy;
-	int numCPUs;
+	private CPUSchedulingPolicy schedulingPolicy;
+	private int numCPUs = 1;
+	private ArrayList<Driver> drivers;
 
 	@Override public void start( Stage primaryStage ) throws Exception {
+		this.drivers = new ArrayList<>();
+
 		Pane contentPane = new Pane();
 
 		// Memory
@@ -43,12 +48,12 @@ public class GUI extends Application {
 
 		// CPUs
 
-		Scene scene = new Scene( contentPane, 1000, 700 );
+		Scene scene = new Scene( contentPane, 1500, 600 );
 		primaryStage.setScene( scene );
 		primaryStage.show();
 	}
 
-	private void initialize() throws Exception {
+	private void reset() throws Exception {
 		TaskManager.INSTANCE.reset();
 		CPU.reset();
 		Driver.reset();
@@ -59,14 +64,33 @@ public class GUI extends Application {
 		clearMemory( disk );
 		this.mmu.reset();
 
+		this.drivers.clear();
+		for ( int i = 0; i < this.numCPUs; i++ ) {
+			this.drivers.add( new Driver( i, disk, mmu, registers, this.schedulingPolicy ) );
+		}
+
 		Driver.loadFile( disk, new File( "src/yeezus/Program-File.txt" ) );
 	}
 
 	private void execute() throws Exception {
+		reset();
 
+		// TODO Enact changes
+
+		Thread[] threads = new Thread[numCPUs];
+
+		// Start Drivers
+		for ( int i = 0; i < numCPUs; i++ ) {
+			threads[i] = new Thread( drivers.get( i ) );
+			threads[i].run();
+		}
+
+		for ( Thread thread : threads ) {
+			thread.join( 500 );
+		}
 	}
 
-	private void clearMemory( Memory memory ) {
+	private void clearMemory( @NotNull Memory memory ) {
 		for ( int i = 0; i < memory.getCapacity(); i++ ) {
 			memory.write( i, new Word( 0 ) );
 		}
